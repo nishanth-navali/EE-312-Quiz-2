449ad46f5731b27522366808e796417a1efb1f73
nishanth-navali <nishanth.navali@utexas.edu>
2020-04-06 18:28:49 -0500

    implement SubsetSum

---
+++ sum.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+bool SubsetSum(int x[], int n, int k, int v) {
+    if (v== 0 && k == 0) return true;
+    if (v != 0 && k == 0) return false;
+    if (n == 0) return false;
+
+    int r1 = 0;
+    if(v >= x[0]) r1 = SubsetSum(x + 1, n - 1, k - 1, v - x[0]);
+    int r2 = SubsetSum(x + 1, n - 1, k, v);
+    if(r1 + r2 > 0) {
+        return true;
+    }
+    return false;
+}
+
+int main(void) {
+
+    return 0;
+}
